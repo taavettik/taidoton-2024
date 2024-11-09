a2447e4fc63232b2b2fd4006e600a7b06845b30a
Hugo Hutri <55588133+hugohutri@users.noreply.github.com>
2024-11-09 17:52:47 +0200

    feat(openai): wip

--- analysis/extract_slack.py
+++ analysis/extract_slack.py
@@ -2,6 +2,66 @@ import csv
 import json
 from typing import Dict, List
 from pydantic import BaseModel
+import os
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+from openai import OpenAI
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+resp = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print("resp", resp)
+
+
+def process_messages(msgs: str):
+    chat_conpletion_response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": """
+Generate a JSON response with toxicity scores for each individual based on their messages. Rate each person on a scale of 0 to 1, where 1 represents high toxicity and 0 represents no toxicity. Provide scores as floating-point values (e.g., 0.25, 0.75) for nuanced results. Follow this format exactly:
+
+{
+  "Person_A": {"toxic": 1.0},
+  "Person_B": {"toxic": 0.0},
+  "Person_C": {"toxic": 0.45}
+}
+
+Do not include any additional text; respond with JSON only.
+
+Messages for analysis:
+"""
+                + msgs,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return chat_conpletion_response.choices[0].message.content
+
+
+# # list engines
+# engines = openai.Engine.list()
+
+# # print the first engine's id
+# # print(engines.data)
+
+# # create a completion
+# completion = openai.chat.completions.create(
+#     engine="gpt-3.5-turbo", prompt="Moi miten menee"
+# )
+
+# # print the completion
+# print(completion.choices[0].text)
 
 
 class RawData(BaseModel):
@@ -39,6 +99,19 @@ class SummaryEntry(BaseModel):
 
 def read_slack_data(file_path: str) -> List[MessageEntry]:
     slack_data: List[MessageEntry] = []
+
+    scores = process_messages(
+        """
+    Taavetti: "I hate you Hugo! You are the worst person I have ever seen!",
+    Hugo: "Sorry :( That was my mistake. I am so sorry.",
+    Taavetti: "I will not forget this! I will kick you out of the company and eat your lunchbox!",
+    Sandra: "Perkele mennään pelaa padelia. Teidän tappelut ei kiinnosta. Perkele!",
+    Late: "Moi taavetti :))) Mä oon AI divisioonan johtaja",
+    Ella: "Moi mä designaan",
+    """
+    )
+    print(scores)
+    return
 
     with open(file_path, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
